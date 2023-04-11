13ead1617e950b0125bca2214fd43007acba5050
Di5gui5edW0lf <spearson@thedckc.com>
2023-04-11 09:33:08 -0700

    changed the randomgame.js file

--- randomGame.js
+++ randomGame.js
@@ -1,13 +1,12 @@
 function randomGame() {
-    let counter = 0;
-    const timer = setInterval(() => {
-      const randNum = Math.random();
-      counter++;
-      if (randNum > 0.75) {
-        console.log(`It took ${counter} tries to find a number greater than 0.75.`);
-        clearInterval(timer);
-      }
-    }, 1000);
-  }
-
-  randomGame()
+  let counter = 0;
+  const resultElem = document.getElementById("result");
+  const timer = setInterval(() => {
+    const randNum = Math.random();
+    counter++;
+    if (randNum > 0.75) {
+      resultElem.innerHTML = `It took ${counter} tries to find a number greater than 0.75.`;
+      clearInterval(timer);
+    }
+  }, 1000);
+} 
